fda01ebefaa37b048344f6b1db411f3fd117d1e1
haleanana <haleanana@gmail.com>
2020-09-18 07:12:35 +0000

    Change layout of showrecipe.html so ingredients and methods are displayed side by side

--- static/js/main.js
+++ static/js/main.js
@@ -3,5 +3,7 @@ $(window).on('resize load', function() {
   if ($(window).width() <= 768) { 
     $("#mobile").removeClass("row");
     $("#mobile").removeClass("row-cols-3")
+    $("#showRecipeMobile").removeClass('col-4')
+    $("#showRecipeMobile2").removeClass('col-8')
   }
 });
