2f26b71a7fdfed5af62d62f7030b271daf7f5f79
haleanana <haleanana@gmail.com>
2020-10-28 09:27:05 +0000

    converted home page images  to webp to optimise load times

--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,3 @@
-/*removes classes to make it responsive on mobile view
-This was done as it was not possible for me to use CSS 
-to do this*/
-
-/*$(window).on('resize load', function() {
-  if ($(window).width() <= 768) { 
-    $("#mobile").removeClass("row");
-    $("#mobile").removeClass("row-cols-3")
-    $("#showRecipeMobile").removeClass('col-4')
-    $("#showRecipeMobile2").removeClass('col-8')
-  }
-});*/
 const FAVOURITE_RECIPES = 'FAVOURITE_RECIPES'
 
 function addFavourites(id, url, recipeName, recipeDescription, recipeImage){
