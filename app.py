c4204006283c957476b6cf318770bb2dd8eb551f
haleanana <haleanana@gmail.com>
2020-11-11 13:28:24 +0000

    debug set to false

--- app.py
+++ app.py
@@ -107,5 +107,5 @@ def internal_server(error):
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), port=int(
         os.environ.get('PORT')),
-        debug=True)
+        debug=False)
 
